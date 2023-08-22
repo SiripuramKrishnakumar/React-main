e2fb79eea89b017079364831b78a863b029a6e96
Krishna Kumar Siripuram <krishnakumar.siripuram@techwave.net>
2023-08-22 23:08:48 +0530

    Routing

--- my-react-app/src/Components/NavBar.js
+++ my-react-app/src/Components/NavBar.js
@@ -1,30 +1,60 @@
 import React from "react";
 import logo from "../logo.svg";
 import { useSelector } from "react-redux";
-
+import { Link, NavLink } from "react-router-dom";
 const NavBar = () => {
+  const counterVal = useSelector((state) => state.counter.counterVal);
 
-    const counterVal = useSelector(state => state.counter);
-
-
-    return(
-        <nav className="navbar bg-body-tertiary">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">
-            <img
-              className="App-logo"
-              src={logo}
-              alt="logo"
-              style={{ height: "15%", width: "15%" }}
-            />
-            React Practice
-          </a>
-          <div>
-            <span className="badge rounded-pill text-bg-success"> <text style={{color:'black'}}> Redux Example Counter Value:</text>   <strong style={{color:'red'}}> {counterVal} </strong> </span>
+  return (
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container-fluid">
+        <Link className="navbar-brand" to="/">
+          <img
+            className="App-logo"
+            src={logo}
+            alt="logo"
+            style={{ height: "15%", width: "15%" }}
+          />
+          React Practice
+        </Link>
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav">
+            <NavLink className="nav-link btn btn-success" aria-current="page" to="/">
+              Home
+            </NavLink>
+            <NavLink className="nav-link btn btn-success" to="/people">
+              People
+            </NavLink>
+            <NavLink className="nav-link btn btn-success" to="/userForm">
+              User Form
+            </NavLink>
+            <NavLink className="nav-link btn btn-success" to="/counter">
+              Add Counter
+            </NavLink>
+            <NavLink
+              className="nav-link disabled"
+              to="#"
+              aria-disabled="true"
+            >
+              Disabled
+            </NavLink>
+            <NavLink
+              className="nav-link btn btn-success"
+              to="abc"
+            >
+              Error Page
+            </NavLink>
           </div>
         </div>
-      </nav>
-    );
-}
+        <span className="badge rounded-pill text-bg-success"></span>
+          <span style={{ color: "black" }}>
+            Redux Example Counter Value:
+          <strong style={{ color: "red" }}> {counterVal} </strong>
+        </span>
+      </div>
+    </nav>
+
+  );
+};
 
 export default NavBar;
